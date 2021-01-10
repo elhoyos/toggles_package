2d905c052a508f8901f6e9a8bb2c62eaf037c0f6
Juan Hoyos <juan.hoyosr@gmail.com>
2021-01-10 13:59:58 -0500

    Sync summary tables with results

--- identification/summary/summary_tables.sql
+++ identification/summary/summary_tables.sql
@@ -11,7 +11,10 @@ create table toggled_repositories
 create table libraries
 (
 	name text not null,
-	languages text not null
+	languages text not null,
+	package_manager_data text,
+	imports_or_usages text,
+	repositories text not null
 );
 
 \copy toggled_repositories(repo_name, library, library_language, number_of_commits) FROM 'summary_tables_toggled_repositories.csv' CSV HEADER
--- identification/summary/summary_tables_queries.sql
+++ identification/summary/summary_tables_queries.sql
@@ -1,20 +1,30 @@
 with total_projects as (
 	select count(*) as total from public.toggled_repositories
 )
-select library, replace(library_language, ',', ', ') as languages, count(*) as "# of projects", count(*)::decimal/(select total from total_projects) as "% of projects"
+select
+  library,
+  replace(library_language, ',', ', ') as languages,
+  count(*) as "# of projects",
+  round(count(*)::decimal/(select total from total_projects) * 100, 2) as "% of projects"
 from public.toggled_repositories
 group by library, library_language
-order by 3 desc
+order by 3 desc;
 
 with total_libraries as (
 	select count(*) as total from libraries
 )
-select replace(languages, ',', ', ') as languages, count(*)::decimal/(select total from total_libraries) as "% of libraries", string_agg(name, ', ') as "libraries"
+select
+  replace(languages, ',', ', ') as languages,
+  count(*)::decimal/(select total from total_libraries) as "% of libraries",
+  string_agg(name, ', ') as "libraries"
 from libraries
 group by languages
-order by 2 desc
+order by 2 desc;
 
-select name || ' (' || replace(languages, ',', ', ') || ')', replace(package_manager_data, ',', ', '), replace(imports_or_usage, ',', ', ')
+select
+  name || ' (' || replace(languages, ',', ', ') || ')',
+  replace(package_manager_data, ',', ', '),
+  replace(imports_or_usages, ',', ', ')
 from libraries l
 inner join (
   select library, library_language, count(*) as total_repositories
@@ -22,5 +32,5 @@ inner join (
   group by library, library_language
 ) t
 on t.library = l.name and t.library_language = l.languages
-order by t.total_repositories desc, imports_or_usage asc
+order by t.total_repositories desc, imports_or_usages asc
 limit 10;
